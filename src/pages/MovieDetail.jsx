88ec6a4e777f69fb8b9d4ba760a465cfb4feb516
James Esguerra <james.a.esguerra@obf.ateneo.edu>
2022-10-06 10:43:38 +0800

    add movie detail overview

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -4,6 +4,8 @@ import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 import movieService from '../actions/movies';
 import MovieBanner from '../components/MovieBanner'
+import { minsToHrsAndMins } from '../utils/time';
+import { getNumberWithCommas } from '../utils/currency';
 
 const MovieDetail = () => {
   const { movieId } = useParams();
@@ -18,18 +20,44 @@ const MovieDetail = () => {
   return (
     <div>
         <MovieBanner hideTxt={true} movieInfo={movieInfo} />
-        <div className='mx-32'>
+        <div className='mx-6 lg:mx-32'>
           <div className='flex gap-x-20'>
+
             <div className='hidden lg:block'>
               <LazyLoadImage src={`https://image.tmdb.org/t/p/w500/${movieInfo.poster_path}`} className="rounded-lg" width={300} height={336} />
             </div>
-            <div className='max-w-xl'>
+
+            <div className='lg:max-w-2xl'>
               <h1 className='text-4xl font-bold font-serif pb-4'>
                 {movieInfo.title}
-                <span className='font-sans text-xl font-light pl-2'>{movieInfo.release_date}</span>
+                <span className='font-sans text-xl font-light pl-2'>{new Date(movieInfo.release_date).getUTCFullYear()}</span>
               </h1>
               <p className='uppercase pb-4'>{movieInfo.tagline}</p>
-              <p>{movieInfo.overview}</p>
+              <p className='pb-10'>{movieInfo.overview}</p>
+
+              <div className="overflow-x-auto">
+                <table className="table w-full">
+                  <tbody>
+                    <tr>
+                      <td>Runtime</td>
+                      <td>{minsToHrsAndMins(movieInfo.runtime)}</td>
+                    </tr>
+                    <tr>
+                      <td>Status</td>
+                      <td>{movieInfo.status}</td>
+                    </tr>
+                    <tr>
+                      <td>Rating</td>
+                      <td>{movieInfo.vote_average}</td>
+                    </tr>
+                    <tr>
+                      <td>Revenue</td>
+                      <td>${getNumberWithCommas(movieInfo.revenue)}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+
             </div>
           </div>
         </div>
---
+++ src/utils/time.js
@@ -0,0 +1,7 @@
+export const minsToHrsAndMins = (n) => {
+    const hours = n / 60;
+    const rHours = Math.floor(hours);
+    const mins = (hours - rHours) * 60;
+    const rMins = Math.round(mins);
+    return `${rHours}hr ${rMins}mins`
+}

b2f14a120700eb96beb499edca08491ab2bc77d7
James Esguerra <james.a.esguerra@obf.ateneo.edu>
2022-10-06 14:57:39 +0800

    fix rendering issue on movie detail page

--- src/actions/movies.js
+++ src/actions/movies.js
@@ -6,6 +6,7 @@ const BASE_URL = `https://api.themoviedb.org/3/movie`;
 
 // https://api.themoviedb.org/3/movie/718930?api_key={{ _.api }}&language=en-US
 // https://api.themoviedb.org/3/movie/popular?api_key=<<api_key>>&language=en-US&page=1
+// https://api.themoviedb.org/3/movie/{movie_id}/credits?api_key=<<api_key>>&language=en-US
 
 const getMovieDetail = (movieId) => {
     const request = axios.get(`${BASE_URL}/${movieId}?api_key=${API_KEY}&language=en-US`);
@@ -20,6 +21,11 @@ const getFeaturedMovie = () => {
 const getFeaturedList = (section) => {
     const request = axios.get(`${BASE_URL}/${section}?api_key=${API_KEY}&language=en-US$page=1`);
     return request.then(response => response.data);
+}
+
+const getCast = (movieId) => {
+    const request = axios.get(`${BASE_URL}/${movieId}/credits?api_key=${API_KEY}&language=en-US`);
+    return request.then(response => response.data['cast']);
 }
 
 const actions = {
--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -10,6 +10,8 @@ import { getNumberWithCommas } from '../utils/currency';
 const MovieDetail = () => {
   const { movieId } = useParams();
   const [movieInfo, setMovieInfo] = useState({});
+  const [genres, setGenres] = useState();
+  const [revenue, setRevenue] = useState();
 
   useEffect(() => {
     movieService
@@ -17,13 +19,20 @@ const MovieDetail = () => {
       .then(data => setMovieInfo(data))
   }, [])
 
+  useEffect(() => {
+    if (Object.keys(movieInfo).length !== 0) {
+      setGenres(movieInfo.genres.map((genre) => genre.name).join(', '))
+      setRevenue(getNumberWithCommas(movieInfo.revenue));
+    }
+  }, [movieInfo])
+
   return (
     <div>
         <MovieBanner hideTxt={true} movieInfo={movieInfo} />
         <div className='mx-6 lg:mx-32'>
           <div className='flex gap-x-20'>
 
-            <div className='hidden lg:block'>
+            <div className='hidden xl:block'>
               <LazyLoadImage src={`https://image.tmdb.org/t/p/w500/${movieInfo.poster_path}`} className="rounded-lg" width={300} height={336} />
             </div>
 
@@ -33,7 +42,14 @@ const MovieDetail = () => {
                 <span className='font-sans text-xl font-light pl-2'>{new Date(movieInfo.release_date).getUTCFullYear()}</span>
               </h1>
               <p className='uppercase pb-4'>{movieInfo.tagline}</p>
-              <p className='pb-10'>{movieInfo.overview}</p>
+              <p className='pb-8'>{movieInfo.overview}</p>
+              
+
+              <div className="tabs tabs-boxed">
+                <a className="tab tab-active">Overview</a> 
+                <a className="tab">Cast</a> 
+              </div>
+
 
               <div className="overflow-x-auto">
                 <table className="table w-full">
@@ -43,17 +59,17 @@ const MovieDetail = () => {
                       <td>{minsToHrsAndMins(movieInfo.runtime)}</td>
                     </tr>
                     <tr>
-                      <td>Status</td>
-                      <td>{movieInfo.status}</td>
+                      <td>Genres</td>
+                      <td>{genres}</td>
                     </tr>
                     <tr>
                       <td>Rating</td>
                       <td>{movieInfo.vote_average}</td>
                     </tr>
-                    {/* <tr>
+                    <tr>
                       <td>Revenue</td>
-                      <td>${getNumberWithCommas(movieInfo.revenue)}</td>
-                    </tr> */}
+                      <td>${revenue}</td>
+                    </tr>
                   </tbody>
                 </table>
               </div>

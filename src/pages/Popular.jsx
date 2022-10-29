826a3126ce17f1eb58104abd7ee60ad7b6dbb067
James Esguerra <james.a.esguerra@obf.ateneo.edu>
2022-10-29 19:40:16 +0800

    fix lint issues with popular page

--- src/pages/Popular.jsx
+++ src/pages/Popular.jsx
@@ -1,10 +1,9 @@
-import { useState, useEffect } from 'react';
-import { useBottomScrollListener } from 'react-bottom-scroll-listener';
-import { BottomScrollListener } from 'react-bottom-scroll-listener';
+import { useState, useEffect } from "react";
+import { useBottomScrollListener } from "react-bottom-scroll-listener";
+import { BottomScrollListener } from "react-bottom-scroll-listener";
 
-
-import movieService from '../actions/movies';
-import MovieCard from '../components/MovieCard';
+import movieService from "../actions/movies";
+import MovieCard from "../components/MovieCard";
 
 const Popular = () => {
 
@@ -16,32 +15,32 @@ const Popular = () => {
 
     movieService
       .getPopularMovies(page)
-      .then(data => setMovies([...movies, ...data]))
-  }
+      .then(data => setMovies([...movies, ...data]));
+  };
 
   const scrollRef = useBottomScrollListener(callback);
 
   useEffect(() => {
     movieService
       .getPopularMovies(page)
-      .then(data => setMovies(data))
-  }, [])
+      .then(data => setMovies(data));
+  }, []);
 
   return (
     <>
       <h1 className='mx-6 mt-16 text-3xl pb-8'>Popular Movies</h1>
-      <BottomScrollListener onBottom={callback}>
+      <BottomScrollListener onBottom={callback} ref={scrollRef}>
         {(scrollRef) => (
           <div
             className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 mb-16 mx-6 gap-4" 
             ref={scrollRef}
           >
-          {movies.map(movie => <MovieCard key={movie.title} movie={movie} />)}
+            {movies.map(movie => <MovieCard key={movie.title} movie={movie} />)}
           </div>
         )}
       </BottomScrollListener>
     </>
-  )
-}
+  );
+};
 
-export default Popular
+export default Popular;

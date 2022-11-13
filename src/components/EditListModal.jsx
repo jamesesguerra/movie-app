c7910873e7924f56e9271f5e012863092337e35f
James Esguerra <james.a.esguerra@obf.ateneo.edu>
2022-11-13 22:39:42 +0800

    add forms to edit list modal and set max width

--- src/components/EditListModal.jsx
+++ src/components/EditListModal.jsx
@@ -2,14 +2,36 @@ const EditListModal = () => {
   return (
     <>
       <label htmlFor="my-modal-6" className="btn btn-sm">Edit List</label>
-
       <input type="checkbox" id="my-modal-6" className="modal-toggle" />
       <div className="modal modal-bottom">
         <div className="modal-box min-h-screen">
-          <h3 className="font-bold text-lg">Congratulations random Internet user!</h3>
-          <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
-          <div className="modal-action">
-            <label htmlFor="my-modal-6" className="btn">Yay!</label>
+          <div className="w-[72rem] mx-auto">
+            <h1 className="font-bold text-3xl">Edit List</h1>
+            <div className="divider"></div> 
+            <form className="form-control w-full">
+              <label className="label">
+                <span className="label-text">List Name</span>
+              </label>
+              <input
+                id="name"
+                name="name"
+                type="text"
+                placeholder="Add a list name"
+                className="input input-bordered w-full"
+              />
+              <label className="label pt-8">
+                <span className="label-text">Description</span>
+              </label>
+              <textarea
+                id="description"
+                name="description"
+                className="textarea textarea-bordered"
+                placeholder="Add a description"
+              ></textarea>
+            </form>
+            <div className="modal-action">
+              <label htmlFor="my-modal-6" className="btn">Yay!</label>
+            </div>
           </div>
         </div>
       </div>

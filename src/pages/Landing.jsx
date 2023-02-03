7cad75471308e1bea85d530df02c5c1161bd920f
James Esguerra <james.a.esguerra@obf.ateneo.edu>
2023-02-03 16:02:03 +0800

    add default labels for school project

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -8,22 +8,24 @@ const Landing = () => {
   }, []);
 
   return (
-    <div className="mx-auto">
-      <div className="hero min-h-full bg-base-100 my-36">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <div className="mockup-window border border-base-300">
-            <div className="px-36 md:px-64 lg:px-64 py-32 border-t border-base-300">Hello!</div>
-          </div>  
-          <div className="max-w-2xl mr-4">
-            <h1 className="text-5xl font-bold text-left">Lorem Ipsum Dolor</h1>
-            <p className="py-6 text-left">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-            <button className="btn btn-primary flex justify-end">Get Started — it's free</button>
+    <div className="h-screen">
+      <div className="mx-auto">
+        <div className="hero min-h-full bg-base-100 my-36">
+          <div className="hero-content flex-col lg:flex-row-reverse">
+            <div className="mockup-window border border-base-300">
+              <div className="px-36 md:px-64 lg:px-64 py-32 border-t border-base-300">Welcome!</div>
+            </div>  
+            <div className="max-w-2xl mr-4">
+              <h1 className="text-5xl font-bold text-left">Sample Movie App</h1>
+              <p className="py-6 text-left">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+              <button className="btn btn-primary flex justify-end">Get Started — it's free</button>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="prose container mx-auto my-16 px-2">
-        <h1 className='text-center'>it's like letterboxd except not really</h1>
+        <div className="prose container mx-auto my-16 px-2">
+          <h1 className='text-center'></h1>
+        </div>
       </div>
     </div>
   );

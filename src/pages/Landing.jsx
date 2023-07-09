72585068e9d166712e63402d8696576f8d190cc9
James Esguerra <james.a.esguerra@obf.ateneo.edu>
2023-07-09 15:11:06 +0800

    fix layout of hero section

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -16,28 +16,15 @@ const Landing = () => {
         <div className="hero min-h-full bg-base-100 mb-36 mt-24">
           <div className="hero-content flex-col lg:flex-row-reverse">
             <div className="">
-              <img src={art} alt="A guy watching a movie" className="" />
+              <img src={art} alt="A guy watching a movie" />
             </div>
-            <div className="max-w-2xl mr-4">
-              <h1 className="text-5xl font-bold text-left">Pellicola</h1>
-              <p className="py-6 text-left">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-              <button className="btn btn-primary flex justify-end">Get Started — it's free</button>
+            <div className="max-w-2xl mr-4 text-center lg:text-left">
+              <h1 className="text-5xl font-bold">Pellicola</h1>
+              <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+              <button className="btn btn-primary lg:mx-auto">Get Started — it's free</button>
             </div>
           </div>
         </div>
-
-        {/* <div className="bg-base-100 my-24">
-          <div className="hero-content flex-col lg:flex-row">
-            <div className="max-w-xs max-h-96">
-              <img src={phone1} alt="" />
-            </div>
-
-            <div className="mr-4 prose">
-              <h2 className="font-bold text-left">It's like letterboxd except not really.</h2>
-              <p className="text-left">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-            </div>
-          </div>
-        </div> */}
 
         <div className="bg-base-100 my-24 px-4 text-center">
           <div className="max-w-xl mx-auto">
